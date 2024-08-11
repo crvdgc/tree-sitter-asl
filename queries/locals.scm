fbbc1cc8e33ab51c12778cc0f0fa3ddbe1f60bb6
crvdgc <ubikium@gmail.com>
2024-08-11 20:37:30 +0100

    chore: add exception local type def

--- queries/locals.scm
+++ queries/locals.scm
@@ -5,6 +5,7 @@
 (type_decl (ty) @local.definition)
 (type_decl (ty "enumeration" (identifier) @local.definition))
 (type_decl (ty "record" (field (identifier) @local.definition)))
+(type_decl (ty "exception" (field (identifier) @local.definition)))
 (parameter (identifier) @local.definition)
 (formal (identifier) @local.definition)
 (storage_decl (identifier) @local.definition)
